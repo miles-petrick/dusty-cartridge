524b01a19aba7ac32695e1304a4f3db532cc64c7
miles-petrick <103614945+miles-petrick@users.noreply.github.com>
2022-10-20 18:28:40 -0500

    19.1 activities (unfinished)

---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,18 @@
+// Create the addNumbers function here.
+function doSomething() {
+    var two = 2;
+    var three = 3;
+    var five = two + three;
+    console.log(five)
+}
+
+doSomething();
+
+// Create the stringConcat function here.
+function stringConcat() {
+    var taco = 'taco';
+    var cat = 'cat';
+    var tacoCat = taco + ' ' + cat;
+    alert(tacoCat)
+}
+stringConcat();
---
+++ 19.1-activities/04_on_function/unsolved/js/index.js
@@ -0,0 +1,17 @@
+// .on event listener documentation - https://api.jquery.com/on/#on-events-selector-data-handler
+
+// Step 1: Hide a div
+$('#button1').on("click", function(){
+    $('.hideMe').slideToggle();
+
+});
+
+// Step 2: Change a color
+$('#button2').on("click", function(){
+
+});
+
+// Step 3: Append content
+$('#button3').on("click", function(){
+
+});
